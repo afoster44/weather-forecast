61f58208083fb76f61747775d9cc84740ecfb620
Alex Foster <alexfost94@gmail.com>
2021-05-28 19:33:11 -0600

    Trying to drill into weather array/wind speed up

--- app/Models/Forecast.js
+++ app/Models/Forecast.js
@@ -4,6 +4,7 @@ export default class Forecast {
             this.feelsLike = data.feels_like.day,
             this.humidity = data.humidity,
             this.wind = data.wind_speed,
+            this.direction = data.wind_deg,
             this.date = new Date(data.dt * 1000).toString().slice(0, 15)
     }
 
@@ -15,15 +16,24 @@ export default class Forecast {
         <div class="col-sm-12">
         <p>${this.date}:</div>
         <div class ="col-sm-12 col-lg-6">
-         <p>Temperature: <span>${this.temperature}</span></p>
+         <p>Temperature: <span>${this.temperature}</span>F</p>
                     
             </div>
             <div class = "col-sm-12 col-lg-6">
-            <p>Feels like: <span>${this.feelsLike}</span></p>
+            <p>Feels like: <span>${this.feelsLike}</span>F</p>
+            </div>
+            <div class = "col-sm-12 col-lg-6">
+            <p>Wind speed: <span> ${this.wind}</span> MPH</p>
             </div>
         </div>       
         </div>
                 
         `
+    }
+
+    get Weather() {
+        let template = ''
+        this.weather.forEach(element => template += element.weather)
+        return template
     }
 }
